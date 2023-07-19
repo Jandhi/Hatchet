367c2049e0d13419dbdfec4982cab472954a018d
Jan Dohring <jan.dohring@zemind.ca>
2023-07-19 15:07:54 -0400

    transpiler start

--- src/stdlib/load.rs
+++
@@ -1,9 +0,0 @@
-use crate::{state::State};
-
-use super::{strings, math, logic};
-
-pub fn load_stdlib(state : &mut State) {
-    math::load(state);
-    strings::load(state);
-    logic::load(state);
-}
