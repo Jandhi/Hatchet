c1b92c771d198428a37561a99eb8fc42c8ed99e1
Jan Dohring <janderkaiser@gmail.com>
2024-07-12 17:05:04 -0400

    stack start

--- src/parser/node.rs
+++ src/parser/node.rs
@@ -1,9 +1,16 @@
 use std::rc::Rc;
 
-use crate::{position::Position, typechecker::hatchet_type::HatchetType};
+use crate::{lexer::lexeme::Lexeme, position::Position, typechecker::hatchet_type::HatchetType};
 
 pub struct Node {
     pub position : Position,
     pub children : Vec<Rc<Node>>,
+    pub node_type : NodeType,
     pub hatchet_type : HatchetType
+}
+
+pub enum NodeType {
+    Root,
+    Lexeme(Lexeme),
+    
 }
--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,11 +1,13 @@
 use std::rc::Rc;
 
-use crate::lexer::lexeme::Lexeme;
+use crate::{lexer::lexeme::Lexeme, position::Position, typechecker::hatchet_type::HatchetType};
 
-use super::node::Node;
+use super::node::{Node, NodeType};
 
 struct Parser<'a> {
-    lexemes : &'a Vec<Lexeme>
+    lexemes : &'a Vec<Lexeme>,
+    counter : usize,
+    stack : Vec<Rc<Node>>,
 }
 
 pub fn parse(lexemes : &Vec<Lexeme>) -> Rc<Node> {
@@ -14,14 +16,47 @@ pub fn parse(lexemes : &Vec<Lexeme>) -> Rc<Node> {
 
 impl<'a> Parser<'a> {
     fn new(lexemes : &'a Vec<Lexeme>) -> Parser<'a> {
-        Parser { lexemes }
+        Parser { 
+            lexemes, 
+            counter: 0, 
+            stack: vec![],
+        }
     }
 
     fn parse(&mut self) -> Rc<Node> {
-        Rc::from(Node{
-            position: todo!(),
-            children: todo!(),
-            hatchet_type: todo!(),
-        })
+        let mut root = Rc::from(Node{
+            position: Position::new(),
+            children: vec![],
+            node_type: NodeType::Root,
+            hatchet_type: HatchetType::Unknown,
+        });
+
+        self.stack.push(root.clone());
+
+        while let Some(lex) = self.get_next_lexeme() {
+            self.stack.push(Rc::from(Node{
+                position: lex.position.clone(),
+                children: vec![],
+                node_type: NodeType::Lexeme(lex),
+                hatchet_type: HatchetType::Unknown,
+            }));
+
+            while self.reduce() {}
+        }
+
+        root
+    }
+
+    fn get_next_lexeme(&mut self) -> Option<Lexeme> {
+        if self.counter < self.lexemes.len() {
+            self.counter += 1;
+            return Some(self.lexemes[self.counter - 1].clone());
+        }
+        
+        None
+    }
+
+    fn reduce(&mut self) -> bool {
+        false
     }
 }
